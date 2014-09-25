20a17d94c9c11974e90d49aca9f3f4ab5775a498
Carl Raymond <carl@orcharddev.com>
2014-09-25 15:43:53 -0400

    Add PREFIX state to support other card readers.
    Some card readers prefix the scanned data with a string like
    "\!STCARD A " before the % character. This can be specified
    in the configuration with the prefixCharacter property. An extra
    state in the state machine will eat up characters between the prefix
    character and the % character.  The contents of the prefix are
    discarded, and are not available to the client.

--- src/jquery.cardswipe.js
+++ src/jquery.cardswipe.js
@@ -18,7 +18,7 @@
 }(function ($) {
 
 	// State definitions:
-	var states = { IDLE: 0, PENDING: 1, READING: 2, DISCARD: 3 };
+	var states = { IDLE: 0, PENDING: 1, READING: 2, DISCARD: 3, PREFIX: 4 };
 
 	// Holds current state
 	var state = states.IDLE;
@@ -31,11 +31,16 @@
 
 	// Keypress listener
 	var listener = function (e) {
+		//console.log(e.which + ': ' + String.fromCharCode(e.which));
 		switch (state) {
+
+			// IDLE: Look for '%', and jump to PENDING.  Otherwise, pass the keypress through.
 			case states.IDLE:
+				//console.log('IDLE');
 				// Look for '%'
 				if (e.which == 37) {
 					state = states.PENDING;
+					//console.log('IDLE -> PENDING');
 					scanbuffer = new Array();
 					processCode(e.which);
 					e.preventDefault();
@@ -43,12 +48,24 @@
 					startTimer();
 				}
 
+				// Look for prefix, if defined
+				if (settings.prefixCode && settings.prefixCode == e.which) {
+					state = states.PREFIX;
+					//console.log('IDLE -> PREFIX');
+					e.preventDefault();
+					e.stopPropagation();
+					startTimer();
+				}
+
 				break;
 
+			// PENDING: Look for A-Z, then jump to READING.  Otherwise, pass the keypress through, reset and jump to IDLE.
 			case states.PENDING:
+				//console.log('PENDING');
 				// Look for format code character, A-Z. Almost always B for cards
 				// used by the general public.
 				if (e.which >= 65 && e.which <= 90) {
+					//console.log('PENDING -> READING');
 					state = states.READING;
 
 					// Leaving focus on a form element wreaks browser-dependent
@@ -64,11 +81,14 @@
 				else {
 					clearTimer();
 					scanbuffer = null;
+					//console.log('PENDING -> IDLE');
 					state = states.IDLE;
 				}
 				break;
 
+			// READING: Copy characters to buffer until newline, then process the scanned characters
 			case states.READING:
+				//console.log('READING');
 				processCode(e.which);
 				startTimer();
 				e.preventDefault();
@@ -78,6 +98,7 @@
 				if (e.which == 13) {
 					clearTimer();
 					state = states.IDLE;
+					//console.log('READING -> IDLE');
 					processScan();
 				}
 
@@ -88,17 +109,43 @@
 				}
 				break;
 
+			// DISCARD: Eat up characters until newline, then jump to IDLE
 			case states.DISCARD:
+				//console.log('DISCARD');
 				e.preventDefault();
 				e.stopPropagation();
 				if (e.which == 13) {
 					clearTimer();
+					//console.log('DISCARD -> IDLE');
 					state = states.IDLE;
 					return;
 				}
 
 				startTimer();
 				break;
+
+			// PREFIX: Eat up characters until % is seen, then jump to PENDING
+			case states.PREFIX:
+				//console.log('PREFIX');
+
+				// If prefix character again, pass it through and return to IDLE state.
+				if (e.which == settings.prefixCode) {
+					//console.log('PREFIX -> IDLE');
+					state = states.IDLE;
+					return;
+				}
+
+				// Eat character.
+				e.preventDefault();
+				e.stopPropagation();
+				// Look for '%'
+				if (e.which == 37) {
+					state = states.PENDING;
+					//console.log('PREFIX -> PENDING');
+					scanbuffer = new Array();
+					processCode(e.which);
+				}
+				startTimer();
 		}
 	};
 
@@ -120,6 +167,7 @@
 
 	// Invoked when the timer lapses.
 	var onTimeout = function () {
+		//console.log('Timeout!');
 		if (state == states.READING) {
 			processScan();
 		}
@@ -183,7 +231,8 @@
 		success: defaultSuccessCallback,
 		error: null,
 		parser: defaultParser,
-		firstLineOnly: false
+		firstLineOnly: false,
+		prefixCharacter: null,
 	};
 
 	// Plugin actual settings
@@ -194,6 +243,15 @@
 	var methods = {
 		init: function (options) {
 			settings = $.extend(defaults, options || {});
+
+			// Is a prefix character defined?
+			if (settings.prefixCharacter) {
+				if (settings.prefixCharacter.length > 1)
+					throw 'PrefixCharacter must be blank or a single character';
+
+				// Convert to character code
+				settings.prefixCode = settings.prefixCharacter.charCodeAt(0);
+			}
 
 			if (settings.enabled)
 				methods.enable();
@@ -211,8 +269,7 @@
 
 	// The extension proper.  Dispatches methods using the usual jQuery pattern.
 	$.cardswipe = function (method) {
-		// Method calling logic
-		// If named method exists, execute it with passed arguments
+		// Method calling logic. If named method exists, execute it with passed arguments
 		if (methods[method]) {
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 		}
